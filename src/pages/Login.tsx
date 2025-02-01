7443d3e6306c72a4ee0c30fcd2b4f833d1e06b39
Aditha abeynayaka <adithaabe@gmail.com>
2025-02-01 14:51:08 +0530

    logging page 2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../services/AuthService";
+import COVER_PHOTO from "../assets/images/book-login-page-cover.jpg";
 
 export default function Login() {
   console.log("Rendering Login Page");
@@ -33,31 +34,65 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold text-center text-gray-700">
-          Login
-        </h2>
-        {error && <p className="text-red-500 text-sm text-center">{error}</p>}
-        <input
-          type="text"
-          placeholder="Email"
-          ref={emailRef}
-          className="w-full p-2 mt-4 border rounded-md"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          ref={passwordRef}
-          className="w-full p-2 mt-2 border rounded-md"
-        />
-        <button
-          className="w-full mt-4 bg-blue-500 text-white py-2 rounded-md"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
+    <>
+      <div className="w-full h-screen flex item-start">
+        {/* First Half */}
+        <div className="relative w-1/2 h-full flex flex-col">
+          <div className="absolute top-[5%] left-[10%] flex flex-col">
+            <h1 className="text-6xl text-white font-extrabold my-4 ">
+              Public Library - Colombo
+            </h1>
+          </div>
+          <img src={COVER_PHOTO} className="w-full h-screen flex flex-col" />
+        </div>
+
+        {/* Second Half */}
+        <div className="w-1/2 h-full flex flex-col p-20 bg-gray-10 justify-center">
+          <div className="w-full flex flex-col">
+            <h1 className="text-3xl font-semibold mb-4 mt-4">Login</h1>
+            <p className="text-base mb-2 ">
+              Welcom back!. Please enter your details.
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col">
+            <input
+              type="email"
+              placeholder="E-mail"
+              ref={emailRef}
+              className="w-full text-black border-b border-black outline-none focus:outline-none bg-none mt-5"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              ref={passwordRef}
+              className="w-full text-black border-b border-black outline-none focus:outline-none bg-none mt-10 mb-20"
+            />
+          </div>
+
+          {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+
+          <div className="w-full flex flex-col">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-6"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-6"
+            >
+              Register
+            </button>
+          </div>
+
+          <div className="w-full flex items-center justify-center">
+            Don't have a account?. Register.
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
